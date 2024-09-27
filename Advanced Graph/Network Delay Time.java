725c672ec314bbf40c5e4223379f602558af54c9
lone-wolf05 <143163978+shubhangdutta96@users.noreply.github.com>
2024-09-27 16:21:46 +0530

    Network Delay Time.java

---
+++ Advanced Graph/Network Delay Time.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> map = new HashMap<>();
+
+        for(int time[] : times) {
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+
+            map.putIfAbsent(u, new ArrayList<>());
+            map.get(u).add(new int[] {v, w});
+        }
+
+        int ans = 0;
+        int delay[] = new int[n+1];
+        Arrays.fill(delay, Integer.MAX_VALUE);
+        delay[k] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1]-b[1]);
+        pq.offer(new int[] {k, 0});
+
+        while(!pq.isEmpty()) {
+            int curr[] = pq.poll();
+            int u = curr[0];
+            int cost = curr[1];
+
+            if(cost > delay[u]) {
+                continue;
+            }
+
+            if(map.containsKey(u)) {
+                for(int i[] : map.get(u)) {
+                    int v = i[0];
+                    int newCost = cost + i[1];
+
+                    if(newCost < delay[v]) {
+                        delay[v] = newCost;
+                        pq.offer(new int[] {v, newCost});
+                    }
+                }
+            }
+
+        }
+        
+        for(int i=1; i<delay.length; i++) {
+            if(delay[i] == Integer.MAX_VALUE) {
+                return -1;
+            }
+            ans = Math.max(ans, delay[i]);
+        }
+
+        return ans;
+    }
+}
