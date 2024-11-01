ff2631572737b0aa169d846f0210b25be810cf82
lone-wolf05 <143163978+shubhangdutta96@users.noreply.github.com>
2024-11-01 23:24:18 +0530

    Maximum Subarray.java

--- Greedy/Maximum Subarray.java
+++ Greedy/Maximum Subarray.java
@@ -13,4 +13,19 @@ class Solution {
 
         return ans;
     }
+
+        // one more better appraoch
+    public int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either extend the existing subarray or start a new subarray at nums[i]
+            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
+            // Update the maximum sum so far
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
 }
